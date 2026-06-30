dcb164620cf49e09ca3d7b6bd33693153a2ebb3e
lebalang26-web <lebalang26@gmail.com>
2026-06-30 12:59:21 +0200

    Delete TaskManager directory

--- TaskManager/TaskManager/Tasks.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE [dbo].[Tasks]
-(
-	[Id] INT IDENTITY(1,1) NOT NULL PRIMARY KEY, 
-    [Title] VARCHAR(100) NOT NULL, 
-    [Description] VARCHAR(200) NOT NULL, 
-    [ReminderDate] DATETIME NULL, 
-    [Status] VARCHAR(50) NOT NULL
-)
